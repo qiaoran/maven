e514ca2e120e7bdcf73b095ab3f3070934616605
qiaoran <gaoshanqr1989@163.com>
2016-06-02 17:49:17 +0800

    创建项目

---
+++ parent/bussiness/src/main/java/org/bussiness/App.java
@@ -0,0 +1,13 @@
+package org.bussiness;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
